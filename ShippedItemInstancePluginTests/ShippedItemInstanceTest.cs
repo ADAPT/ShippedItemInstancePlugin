fcaab10eb4e7c523f7e46313a43b4f0cd8260149
Kelly Nelson <knelson@farmbeltnorth.com>
2021-04-27 15:37:50 -0500

    Adding sample data, enabling tests, adjusting case sensitivity on several import methods

---
+++ ShippedItemInstancePluginTests/ShippedItemInstanceTest.cs
@@ -0,0 +1,80 @@
+﻿using AgGateway.ADAPT.ApplicationDataModel.ADM;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ShippedItemInstancePluginTests
+{
+    public class ShippedItemInstanceTestData
+    {
+        private IList<ApplicationDataModel> _models;
+        internal IList<ApplicationDataModel> Models
+        {
+            get
+            {
+                if (_models == null)
+                {
+                    AgGateway.ADAPT.ShippedItemInstancePlugin.Plugin shippedItemInstancePlugin = new AgGateway.ADAPT.ShippedItemInstancePlugin.Plugin();
+                    _models = shippedItemInstancePlugin.Import(@"..\..\..\..\SampleData");
+                }
+                return _models;
+            }
+        }
+    }
+
+    [CollectionDefinition("ShippedItemInstanceTestData")]
+    public class TestData : ICollectionFixture<ShippedItemInstanceTestData>
+    {
+    }
+
+    [Collection("ShippedItemInstanceTestData")]
+    public class ShippedItemInstanceTest
+    {
+        ShippedItemInstanceTestData _testData;
+        public ShippedItemInstanceTest(ShippedItemInstanceTestData data)
+        {
+            _testData = data;
+        }
+
+        [Fact]
+        public void Products()
+        {
+            Assert.Equal(6, _testData.Models[0].Catalog.Products.Count);
+            Assert.Equal(6, _testData.Models[0].Catalog.PackagedProducts.Count);
+            Assert.Equal(6, _testData.Models[0].Catalog.PackagedProductInstances.Count);
+
+            Assert.Equal(10, _testData.Models[1].Catalog.Products.Count);
+            Assert.Equal(10, _testData.Models[1].Catalog.PackagedProducts.Count);
+            Assert.Equal(10, _testData.Models[1].Catalog.PackagedProductInstances.Count);
+
+            Assert.Equal(3, _testData.Models[2].Catalog.Products.Count);
+            Assert.Equal(3, _testData.Models[2].Catalog.PackagedProducts.Count);
+            Assert.Equal(3, _testData.Models[2].Catalog.PackagedProductInstances.Count);
+        }
+
+        [Fact]
+        public void Brands()
+        {
+            Assert.Equal(1, _testData.Models[0].Catalog.Brands.Count);
+            Assert.Equal(1, _testData.Models[1].Catalog.Brands.Count);
+            Assert.Equal(1, _testData.Models[2].Catalog.Brands.Count);
+        }
+
+        [Fact]
+        public void Growers()
+        {
+            Assert.Equal(1, _testData.Models[0].Catalog.Growers.Count);
+            Assert.Equal(1, _testData.Models[1].Catalog.Growers.Count);
+            Assert.Equal(1, _testData.Models[2].Catalog.Growers.Count);
+        }
+
+        [Fact]
+        public void Crops()
+        {
+            Assert.Equal(1, _testData.Models[0].Catalog.Crops.Count);
+            Assert.Equal(1, _testData.Models[1].Catalog.Crops.Count);
+            Assert.Equal(1, _testData.Models[2].Catalog.Crops.Count);
+        }
+    }
+}
